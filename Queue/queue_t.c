8b6db12825e68111cf3b6a1829404d13432bee61
Vinay <vhiremath4@gmail.com>
2012-03-25 01:45:48 -0500

    Started the implementation file for queue.

--- Queue/queue_t.c
+++ Queue/queue_t.c
@@ -6,3 +6,30 @@
  *
  *  Description: Implementation of the queue data structure.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "queue_t.h"
+
+
+
+/*  O(1) OPERATION
+ *  --------------
+ *  Initializes the queue with a size of 0. The initializer
+ *  ASSUMES MEMORY HAS ALREADY BEEN ALLOCATED FOR THE PARAMETER
+ *  Q.
+ */
+
+void queue_init(queue_t *q){
+
+    // NULL-check the parameter
+    if (!q)
+        return;
+
+    q->size = 0;
+
+    // initialize the stacks
+    stack_init(&in);
+    stack_init(&out);
+}
