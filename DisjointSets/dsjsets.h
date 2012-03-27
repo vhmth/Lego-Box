32d064e41e166569023e3b3d53f853387483d3c0
Vinay <vhiremath4@gmail.com>
2012-03-26 20:02:43 -0500

    Began work on a disjoint-sets datastructure.

---
+++ DisjointSets/dsjsets.h
@@ -0,0 +1,33 @@
+/*
+ *
+ *  Author: Vinay Hiremath
+ *          vhiremath4@gmail.com
+ *          www.vhmath.com
+ *  Description: Header file for a disjoint-sets data structure.
+ */
+
+#ifndef DSJSETS_H__
+#define DSJSETS_H__
+
+// the disjoint-sets struct
+typedef struct _dsjsets {
+
+    // the array that will contain all the sets
+    int *dsets;
+
+    // the number of sets
+    int size;
+} dsjsets;
+
+// Functionals.
+void dsjsets_addelems(const int n);
+int dsjsets_find(const int x);
+void dsjsets_union(int a, int b);
+int dsjsets_size();
+
+// Utility functions.
+void dsjsets_init(dsjsets *d);
+void dsjsets_print(dsjsets *d);
+void dsjsets_destroy(dsjsets *d);
+
+#endif
